885df1ee38d7ded12d2460cfb1c9d3b8750f64a0
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-07 21:51:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+//your JS code here. If required
+function removecolor (){
+	var colorSelect = document.getElementById("colorSelect");
+	var colorIndex = colorSelect.selectedIndex;
+
+	if(colorIndex !== -1)
+	{
+		colorSelect.remove(colorIndex);
+	}
+}
